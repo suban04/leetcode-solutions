cab88e218ad677b16fede5b0eb0a7b026d25bd9f
Suban Shaikh <subanshaikh134@gmail.com>
2026-09-21 17:55:49 +0530

    Time: 1 ms (99.61%), Space: 42.1 MB (99.43%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,28 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+
+        // Max Heap
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        // Add all stones
+        for (int stone : stones) {
+            pq.offer(stone);
+        }
+
+        // Smash stones
+        while (pq.size() > 1) {
+
+            int first = pq.poll();   // Largest
+            int second = pq.poll();  // Second Largest
+
+            if (first != second) {
+                pq.offer(first - second);
+            }
+        }
+
+        return pq.isEmpty() ? 0 : pq.poll();
+    }
+}
